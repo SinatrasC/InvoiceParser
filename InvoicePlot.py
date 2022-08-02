78657fa5f2b868c6112d2a85ec8955d0bf7a2be7
Sinatras <93725039+SinatrasC@users.noreply.github.com>
2022-08-02 08:36:09 +0300

    Added error handling and webpage creation
    + Added error handling for file operations
    + Added auto webpage creation with all plots

--- InvoicePlot.py
+++ InvoicePlot.py
@@ -4,6 +4,8 @@ import numpy as np
 import configparser
 import sqlite3
 import re
+import os
+import sys
 
 #  Load Config
 config = configparser.ConfigParser()
@@ -12,7 +14,13 @@ config.read('config.ini')
 flags = config.items( "Templates" )
 for key, flag in flags:
     #for each flag read sum price and date from db and plot it
-    conn = sqlite3.connect('invoices.db')
+    #try to connect db catch error if db not exist
+    try:
+        conn = sqlite3.connect('invoices.db')
+    except:
+        print("DB could not be opened or created, please check if file is writeable or appropriate permissions are set")
+        sys.exit(1)
+
     c = conn.cursor()
     c.execute("SELECT sum, date FROM summaries WHERE company = ?", (flag,))
     rows = c.fetchall()
@@ -32,6 +40,19 @@ for key, flag in flags:
         plt.xlabel('Date')
         plt.ylabel('Price')
         plt.tight_layout(pad=5)
-        plt.savefig('invoice_plot_' + flag + '.png', format = "png", dpi = 300, bbox_inches = "tight", pad_inches=0.5)
+        plt.savefig('plots/invoice_plot_' + flag + '.png', format = "png", dpi = 300, bbox_inches = "tight", pad_inches=0.5)
         plt.show()
         plt.close()
+
+try:
+    html_file = open('invoice_plot.html', 'w')
+except:
+    print("File could not be opened, please check if file is writeable or appropriate permissions are set")
+    sys.exit()
+
+html_file.write('<html><head><title>Invoice Plot</title></head><body>')
+for file in os.listdir("plots/"):
+    if file.endswith(".png"):
+        html_file.write('<img src="' + "plots/" + file + '" alt="' + file + '" width="75%">')
+html_file.write('</body></html>')
+html_file.close()
